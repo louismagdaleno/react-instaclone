b206741e0616c1e33d470257389dc7de3c424651
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-08 14:35:04 -0800

    Added SubComponents for SearchBar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+    return (
+        <header>
+            <Logos />
+            <SearchBox />
+            <UserIcons />
+        </header>
+    );
+}
+
+export default SearchBar;
